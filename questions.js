0c0a773b33a392d69a5ad343c0d742e7d6869d2c
MillsPat <millspat71@gmail.com>
2021-08-29 19:58:58 -0400

    new

---
+++ questions.js
@@ -0,0 +1,33 @@
+//Questions will reflect the show "The Office"
+
+var questions = [
+    {
+        title: "How Many Players Can You Have on the Field at one time?",
+        choices: ["10", "12", "11", "13"],
+        answer: "11"
+    },
+    {
+        title: "What is the ideal operation time for an NFL snap and Punt process?",
+        choices: ["1.5 seconds", "1.8 Seconds", "2 Seconds", "2.5 Seconds"],
+        answer: "LittleKidLover"
+    },
+    {
+        title: "What NFL Team used to be in St. Louis but is not in LA?",
+        choices: ["The Rams", "The Steelers", "The Raiders", "The Jets"],
+        answer: "The Rams"
+    },
+    {
+        title: "What is the average hang time for an SEC punters kick?",
+        choices: ["3 Seconds", "4 Seconds", "5 Seconds", "2.5 SEconds"],
+        answer: "4 Seconds"
+    },
+    {
+        title: "How far does the holder set up from the snapper during an extra point?",
+        choices: ["10 Yards", "8 Yards", "7 Yards", "9 Yards"],
+        answer: "7 Yards"
+    },
+
+
+];
+//Just checking my console log :)
+console.log(questions);
